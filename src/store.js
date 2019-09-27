3d168b499a8f218517063f526e33df0d15614453
Michael <michael.zhou42@gmail.com>
2019-09-27 11:40:19 -0400

    added data and routes

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+// Creating a new vuex store which is a global object, so any functions or variables, data, anything that you want to use in multiple parts of the app can all be stored here and then imported into whatever component you want
 Vue.use(Vuex)
 
 export default new Vuex.Store({
---
+++ src/views/Dogs.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Dogs for Adoption</h1>
+    <b-table striped hover :items="dogs"></b-table>
+    </div>
+</template>
+
+<script>
+import dogs from '@/data/dogs' // the @ symbol is a shortcut from the Vue CLI that is a reference to the source directory, so you dont have to do '../../../data/cats'
+export default {
+    data() {
+        return {
+            dogs
+        }
+    }
+}
+</script>
